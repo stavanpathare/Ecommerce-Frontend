fadda80b8d38e7c6f70e97290571f364581cd97d
Stavan Pathare <stavanpathare@gmail.com>
2026-05-20 19:53:40 +0530

    updated Footer

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-window.API_BASE = "https://rajaformens-backend.onrender.com";
-// window.API_BASE = "http://localhost:5000";
+// window.API_BASE = "https://rajaformens-backend.onrender.com";
+window.API_BASE = "http://localhost:5000";
